9c6d85e87374b1d028ddbff5c65d9df193d02e49
unknown <1031966036@qq.com>
2020-11-27 14:37:58 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<!--
+ * @Author: your name
+ * @Date: 2020-11-27 14:13:36
+ * @LastEditTime: 2020-11-27 14:24:05
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \vue_reception\src\App.vue
+-->
+<template>
+  <div id="app">
+    Hello,world
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+};
+</script>
+
+<style></style>
